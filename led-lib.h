b68fcefa6017e974ee71a1018ca722f68c6e6016
Владислав Голосной <vladikgolosnoi@Vladislavs-MacBook-Air.local>
2025-10-04 09:50:12 +0300

    1

--- led-lib.h
+++ led-lib.h
@@ -16,20 +16,41 @@ public:
 // Кодировщик / декодировщик
 class LedIntr {
 public:
-    LedIntr(IGpioAdapter* adapter, int txPin = -1, int rxPin = -1, unsigned long bitDelayUs = 500);
+    LedIntr(IGpioAdapter* adapter, int txPin = -1, int rxPin = -1, unsigned long bitDurationUs = 1000);
 
     void send(const String& text);
-    void receiveLoop();
+
+    void setBitDuration(unsigned long bitDurationUs);
+    unsigned long bitDuration() const { return _bitDurationUs; }
+
+    void setThreshold(uint16_t threshold);
+    uint16_t threshold() const { return _threshold; }
+
+    void setSamplesPerBit(size_t samples);
+    size_t samplesPerBit() const { return _samplesPerBit; }
+
+    void autoCalibrate(size_t sampleCount = 250, unsigned long sampleDelayUs = 500);
+
+    bool receiveMessage(String& out, size_t maxChars = 64, unsigned long startTimeoutMs = 5000);
+
+    void receiveLoop(Stream& output = Serial);
 
 private:
     IGpioAdapter* _adapter;
     int _txPin;
     int _rxPin;
-    unsigned long _bitDelay;
-    int _threshold = 500;
+    unsigned long _bitDurationUs;
+    uint16_t _threshold;
+    size_t _samplesPerBit;
+    unsigned long _sampleSpacingUs;
+    unsigned long _pollDelayUs;
 
     void sendBit(uint8_t bit);
-    uint8_t receiveBit();
+    uint16_t sampleAnalog();
+    void waitInterval(unsigned long durationUs);
+    void updateTiming();
+    bool waitForStartBit(unsigned long timeoutMs);
+    bool receiveByte(uint8_t& byteOut, unsigned long startTimeoutMs);
 };
 
 #endif
